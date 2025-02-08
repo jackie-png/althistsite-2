bacb94c99b8fb2e3c811a71d48121b43dd969b8a
jackie-png <jackylam19@hotmail.com>
2025-02-08 14:11:20 -0500

    changed the sizing on the landing page

--- src/app/page.js
+++ src/app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
   }, [scrollYProgress]);
   
     return (
-      <div className="bg-coal relative" ref={ref}>
+      <div className="bg-coal relative min-h-screen" ref={ref}>
         {!last && <div className="absolute z-20 bottom-0 w-full h-1 flex justify-center items-center">
           <motion.div 
             initial={{width: 0}}
@@ -60,7 +60,7 @@ export default function Home() {
               animate={{
                 scale: (isInView ? 1 : 0)
               }}
-              className="bg-darkRuby w-44 h-44 flex justify-center items-center rounded-full shadow-lg">
+              className="bg-darkRuby w-32 h-32 flex justify-center items-center rounded-full shadow-lg">
                 <motion.div
                   initial={{scale: 0}}
                   animate={{
@@ -69,8 +69,8 @@ export default function Home() {
                   transition={{
                     delay: 0.2
                   }}
-                  className="bg-white w-32 h-32 flex justify-center items-center rounded-full">
-                  <h1 className="text-coal text-4xl font-bold">{date}</h1>
+                  className="bg-white w-24 h-24 flex justify-center items-center rounded-full">
+                  <h1 className="text-coal text-2xl font-bold">{date}</h1>
                 </motion.div>
             </motion.div>
         </div>
@@ -92,8 +92,8 @@ export default function Home() {
                 duration: 0.5,
                 delay: 0.3
               }}
-              className="flex w-2/3 justify-center items-center self-center justify-self-center text-7xl font-bold">
-              <h1 className="underline-offset-8s  underline">{title}</h1>
+              className="flex w-2/3 justify-center items-center self-center justify-self-center text-4xl font-bold">
+              <h1 className="underline-offset-8s underline">{title}</h1>
             </motion.div>
             <div className="bg-gradient-to-b from-transparent to-coal to-70% flex items-center justify-center gap-10">
                 <motion.div 
@@ -111,7 +111,7 @@ export default function Home() {
                     ease: "easeInOut"
                   }}
                   className="border border-soot rounded p-2 max-w-[50%] backdrop-blur-sm">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam quam nulla porttitor massa id neque aliquam. Integer malesuada nunc vel risus commodo viverra maecenas. Donec adipiscing tristique risus nec feugiat. Ornare massa eget egestas purus viverra accumsan. Vitae congue mauris rhoncus aenean. Enim eu turpis egestas pretium. Justo nec ultrices dui sapien eget mi proin. Purus ut faucibus pulvinar elementum integer. Massa sed elementum tempus egestas.</p>
+                    <p className="text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam quam nulla porttitor massa id neque aliquam. Integer malesuada nunc vel risus commodo viverra maecenas. Donec adipiscing tristique risus nec feugiat. Ornare massa eget egestas purus viverra accumsan. Vitae congue mauris rhoncus aenean. Enim eu turpis egestas pretium. Justo nec ultrices dui sapien eget mi proin. Purus ut faucibus pulvinar elementum integer. Massa sed elementum tempus egestas.</p>
                 </motion.div>
                 <motion.button
                   initial={{
@@ -126,7 +126,7 @@ export default function Home() {
                     ease: "easeInOut"
                   }}
                   onClick={()=>router.push(link)}
-                  className="flex items-center gap-2 bg-ruby rounded p-2 transition-transform duration-200 hover:scale-110">
+                  className="flex items-center gap-2 bg-ruby rounded p-2 transition-transform duration-200 text-xs hover:scale-110">
                     Read More
                     <FontAwesomeIcon icon={faArrowRight}/>
                 </motion.button>                        
@@ -142,13 +142,13 @@ export default function Home() {
   return (
     <div className="min-h-screen text-snow bg-snow">
       <div 
-        className="relative grid grid-rows-4 grid-cols-1 h-screen bg-cover bg-no-repeat justify-center" 
+        className="relative grid grid-rows-4 grid-cols-1 min-h-screen bg-cover bg-no-repeat justify-center" 
         style={{
             backgroundImage: 'url("/images/KRworld1951V2_1_3.jpg")',
             backgroundPosition: "center"
         }}>
           <div className="text-[290px] row-span-2 justify-self-center flex items-center">
-            <div className="h-[300px] w-[300px] flex self-center  ">
+            <div className="h-[250px] w-[250px] flex self-center">
               <Logo/>              
             </div>
           </div>
@@ -162,8 +162,8 @@ export default function Home() {
               exit={{opacity: 0}}
               className="bg-coal flex flex-col row-span-1 w-9/12 h-full bg-opacity-50 rounded self-center gap-4 justify-self-center px-8 py-4">
                 <h1 className="text-2xl font-bold text-center">About this website</h1>
-                <h3 className="mb-4 text-lg">This was a Hearts of Iron IV campaign that I enjoyed a lot so I turned it into a website and documented it like a history website in this world</h3>
-                <h3 className="text-lg">The campaign took place in the mod Kaiserreich which explores a world built on the premise of “What if Germany won World War 1?”. When the devs updated the German focus tree I had such a fun time conquering the world, so much that this was made as a memento.</h3>
+                <h3 className="mb-4 text-sm">This was a Hearts of Iron IV campaign that I enjoyed a lot so I turned it into a website and documented it like a history website in this world</h3>
+                <h3 className="text-sm">The campaign took place in the mod Kaiserreich which explores a world built on the premise of “What if Germany won World War 1?”. When the devs updated the German focus tree I had such a fun time conquering the world, so much that this was made as a memento.</h3>
               </motion.div>
               :
               <motion.div
@@ -171,7 +171,7 @@ export default function Home() {
               animate={{opacity: 1}}
               transition={{duration: 0.2}}
               exit={{opacity: 0}}
-              className="text-7xl font-bold select-none justify-self-center justify-center text-center self-center">
+              className="text-5xl font-bold select-none justify-self-center justify-center text-center self-center">
               <h1>Rise of the German Hegemony</h1>
             </motion.div>
             }            
@@ -189,33 +189,33 @@ export default function Home() {
 
 
       </div>
-        <div className="flex justify-center items-center bg-darkRuby h-32 text-6xl font-bold "
+        <div className="flex justify-center items-center bg-darkRuby h-32 text-4xl font-bold "
         >
             <h1 className="border py-4 px-8 tracking-wider rounded">Timeline of Events</h1>
         </div>
         <div className="relative">
-            <div className="relative grid grid-rows-[100vh_100vh_100vh_100vh] z-10">
+            <div className="relative grid grid-rows-[max-content_max-content_max-content_max-content] z-10">
                 <TimelineSection date={1936} title={"The State of the World"} image={"/images/treatyLondon.jpg"} link={"/1936-1940"}/>
                 <TimelineSection date={1940} title={"The Clash between Hammers and Claws"} image={"/images/image2.png"} link={"/1940-1944"}/>
                 <TimelineSection date={1944} title={"The War Beyond Europe"} image={"/images/russia.png"} link={"/1944-1948"}/>
                 <TimelineSection date={1948} title={"Rise of a New World Order"} image={"/images/northAmerica.png"} link={"/1948-1951"} last/>
             </div>        
         </div>
-        <div className="flex justify-center items-center bg-darkRuby h-32 text-6xl font-bold ">
+        <div className="flex justify-center items-center bg-darkRuby h-32 text-4xl font-bold ">
         <h1 className="border py-4 px-8 tracking-wider rounded">Peace Treaties</h1>
         </div>
         <div className="bg-soot h-screen relative">
             <div className="absolute top-0 left-0 w-full h-screen bg-coal z-10 bg-opacity-30"></div>
             <div className="absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-transparent to-coal z-20 flex justify-center items-center">
                 <div className="max-w-[50%] border-t-2 border-l-2 border-r-2 p-4 flex flex-col justify-center items-center gap-4 border-soot backdrop-blur-sm rounded-tl rounded-tr">
-                    <p className="text-2xl">
+                    <p className="text-sm">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam quam nulla porttitor massa id neque aliquam. Integer malesuada nunc vel risus commodo viverra maecenas. Donec adipiscing tristique risus nec feugiat. Ornare massa eget egestas purus viverra accumsan. Vitae congue mauris rhoncus aenean. Enim eu turpis egestas pretium. Justo nec ultrices dui sapien eget mi proin. Purus ut faucibus pulvinar elementum integer. Massa sed elementum tempus egestas.
                     </p>
                     <button 
                       className="flex items-center gap-4 text-2xl font-bold bg-ruby py-2 px-4 rounded transition-transform duration-200 hover:scale-110"
                       onClick={()=>router.push("/peace-treaties")}
                       >
-                        <p className="text-2xl">See More</p>
+                        <p className="text-sm">See More</p>
                         <FontAwesomeIcon icon={faArrowRight}/>
                     </button>
                 </div>
