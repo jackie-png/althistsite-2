082566062bb23a376a2f2cbffa6230712103e568
jackie-png <jackylam19@hotmail.com>
2025-02-17 18:12:42 -0500

    improved the map on mobile

--- src/app/peace-treaties/page.js
+++ src/app/peace-treaties/page.js
@@ -3,19 +3,52 @@ import { SelectableCountries2 } from "@/app/components/SelectableCountries2";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 export default function page(){
-    const [currentHovered, setCurrentHovered] = useState("Hover over the map to select a treaty")
+    const [currentHovered, setCurrentHovered] = useState("Click the coloured territory to select a treaty")
     const router = useRouter();
+    const [isMobile, setMobile] = useState(false)
+    const [link, setLink] = useState("")
 
-    function goToLink(link){
-        router.push(`/articles/peace-treaties/${link}`)
+    console.log(link === "")
+
+    function goToLink(links){
+        router.push(`/peace-treaties/${link}`)
     }
 
     useEffect(()=>{
         console.log(currentHovered)
     },[currentHovered])
+
+    useEffect(()=>{
+        function checkMobile(){
+            console.log(window.innerWidth)
+            if (window.innerWidth < 768){
+                setMobile(true)
+            } else {
+                setMobile(false)
+            }
+        }
+
+        checkMobile();
+
+        window.addEventListener("resize", checkMobile)
+
+        return ()=>{
+            window.removeEventListener("resize", checkMobile)
+        }
+    },[])
+
+    function handleMobilePress(location){
+        const locationSplit = location.split("-").map((ele) => (ele === "of" ? ele : ele[0].toUpperCase() + ele.substring(1)));
+        console.log(locationSplit)
+        setCurrentHovered(locationSplit.reduce((prev, ele) => prev + " " + ele, "" ))
+        setLink(location)
+    }
+
     return(
-        <div>
+        <div className="relative">
             <div className="pt-24 pb-4 bg-darkRuby flex justify-center text-snow">
                 
                 <motion.h1 
@@ -26,13 +59,25 @@ export default function page(){
                         duration: 0.3,
                         ease: "easeInOut"
                     }}
-                    className="text-4xl tracking-wide font-bold">
+                    className="text-sm md:text-2xl tracking-wide font-bold">
                     {currentHovered}
                 </motion.h1>
             </div>
-            <div className="min-h-screen bg-[#0B0B2B]">
-                <SelectableCountries2 onHoverStart={setCurrentHovered} onClick={goToLink}/>
+            <div className="bg-[#0B0B2B] overflow-auto relative">
+                <div className="w-[1200px] md:w-full relative z-0 top-0">
+                    {!isMobile ? 
+                        <SelectableCountries2 onHoverStart={setCurrentHovered} onClick={goToLink}/>
+                        :
+                        <SelectableCountries2 onHoverStart={setCurrentHovered} onClick={handleMobilePress}/>
+                    }
+                </div>
             </div>
+            {link !== "" && <motion.div className={`flex md:none w-full items-end justify-center absolute z-10 bottom-10 left-0`}>
+                <button className="flex items-center justify-center gap-2 p-2 bg-ruby rounded" onClick={()=>goToLink(link)}>
+                    Go to Article
+                    <FontAwesomeIcon icon={faArrowRight} className="w-4 h-4"/>
+                </button>                                
+            </motion.div>}
         </div>
     )
 }
