6176992e7de5b85f936a39368f13fcfa24429f25
jackie-png <jackylam19@hotmail.com>
2025-03-25 12:15:36 -0400

    set a height an dwidth

--- src/app/components/DisplayArticle.js
+++ src/app/components/DisplayArticle.js
@@ -107,6 +107,8 @@ export default function DisplayArticle({data, articleHeadings = []}){
                                     <img 
                                         src={src} 
                                         alt={alt} 
+                                        width={1920}
+                                        height={1080}
                                         className="rounded-lg shadow-lg border-coal border-2 w-11/12 md:w-3/4 lg:w-1/2 cursor-pointer"
                                         loading="eager"
                                         onClick={()=>{
