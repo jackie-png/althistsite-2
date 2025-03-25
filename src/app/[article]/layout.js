f82e5fbd388c886a72b91a3a4368634f38112bfc
jackie-png <jackylam19@hotmail.com>
2025-03-25 12:21:48 -0400

    removed progress bar

--- src/app/[article]/layout.js
+++ src/app/[article]/layout.js
@@ -4,33 +4,33 @@ import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import ArticleContext from "@/app/context/ArticleContext";
 export default function ArticleLayout({children}){
-    const {scrollYProgress} = useScroll()
-        const [height,setHeight] = useState(0);
-        const [hasReachedBottom, setHasReached] = useState(false);
+    // const {scrollYProgress} = useScroll()
+    //     const [height,setHeight] = useState(0);
+    //     const [hasReachedBottom, setHasReached] = useState(false);
     
-      useEffect(() => {
-        return scrollYProgress.onChange((value) => {
-          console.log("scrollYProgress:", value); // This will return a number (0 to 1)
-          if(!hasReachedBottom){
-            setHeight(value);
-          }
-        });
-      }, [scrollYProgress]);
+    //   useEffect(() => {
+    //     return scrollYProgress.onChange((value) => {
+    //       console.log("scrollYProgress:", value); // This will return a number (0 to 1)
+    //       if(!hasReachedBottom){
+    //         setHeight(value);
+    //       }
+    //     });
+    //   }, [scrollYProgress]);
 
-      useEffect(()=>{
-        console.log(height)
-        if(height >= 0.9995){
-            setHasReached(true)
-        } else {
-            setHasReached(false)
-        }
-      },[height])
+    //   useEffect(()=>{
+    //     console.log(height)
+    //     if(height >= 0.9995){
+    //         setHasReached(true)
+    //     } else {
+    //         setHasReached(false)
+    //     }
+    //   },[height])
 
     return (<div>   
-        <motion.div
+        {/* <motion.div
             className={`fixed top-0 left-0 origin-top-left z-[9999] w-full h-2 bg-ruby ${hasReachedBottom ? "rounded-none" : "rounded-r-full"}`}
             style={{width: `${height*100}%`}}
-        ></motion.div>
+        ></motion.div> */}
 
         <ArticleContext>
           {children}
